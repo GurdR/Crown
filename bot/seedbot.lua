f0ac283080b558925e4c1101c5711654239d8deb
GurdR <sup.gurdr@gmail.com>
2016-02-28 18:45:18 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {218766009,121220432,0,tonumber(our_id)},--Sudo users
+    sudo_users = {218766009,121220432,127971239,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
